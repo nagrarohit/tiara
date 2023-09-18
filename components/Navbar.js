ae051824e200aa0e33d9bc6c661eb3137d7765c3
nagrarohit <rohit.nagra7861@gmail.com>
2023-09-18 23:04:43 +0530

    added necklace

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,25 +5,27 @@ import { HiShoppingCart } from "react-icons/hi";
 
 const Navbar = () => {
   return (
-    <div class=" flex  flex-col md:flex-row md:justify-normal justify-center items-center py-2">
-      <div class="logo mx-5">
-        <Image width={200} height={25} src="/textlogo.png" alt=" "></Image>
+    <div className=" flex  flex-col md:flex-row md:justify-normal justify-center items-center py-2 shadow-xl">
+      <div className="logo mx-5">
+        <Link href={"/"}>
+          <Image width={200} height={25} src="/textlogo.png" alt=" "></Image>
+        </Link>
       </div>
-      <div class="nav">
-        <ul class="flex  items-center space-x-2 font-bold md:text-xl ">
-          <Link href={"/"}>
+      <div className="nav">
+        <ul className="flex  items-center space-x-4 font-bold md:text-xl ">
+          <Link href={"/necklace"}>
             <li>Necklace</li>
           </Link>
-          <Link href={"/"}>
+          <Link href={"/rings"}>
             <li>Rings</li>
           </Link>
-          <Link href={"/"}>
+          <Link href={"/bangles"}>
             <li>Bangles</li>
           </Link>
         </ul>
       </div>
-      <div class="cart absolute right-0 top-9 mx-5 ">
-        <HiShoppingCart class="text-xl md:text-4xl "></HiShoppingCart>
+      <div className="cart absolute right-0 top-9 mx-5 ">
+        <HiShoppingCart className="text-xl md:text-4xl "></HiShoppingCart>
       </div>
     </div>
   );
--- pages/necklace.js
+++ pages/necklace.js
@@ -1,7 +1,168 @@
 import React from "react";
 
 const Necklace = () => {
-  return <div>Necklace</div>;
+  return (
+    <div>
+      <section className="text-gray-600 body-font ">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative  rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/necklace/nomodel.webp"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Necklace
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1">$16.00</p>
+                <p className="mt-1">S,M,L</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative  rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/necklace/nomodel.webp"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Necklace
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Shooting Stars
+                </h2>
+                <p className="mt-1">$21.15</p>
+                <p className="mt-1">S,M,L</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative  rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/necklace/nomodel.webp"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Necklace
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Neptune
+                </h2>
+                <p className="mt-1">$12.00</p>
+                <p className="mt-1">S,M,L</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative  rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/necklace/nomodel.webp"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Necklace
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The 400 Blows
+                </h2>
+                <p className="mt-1">$18.40</p>
+                <p className="mt-1">S,M,L</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative  rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/necklace/nomodel.webp"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Necklace
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1">$16.00</p>
+                <p className="mt-1">S,M,L</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative  rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/necklace/nomodel.webp"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Necklace
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Shooting Stars
+                </h2>
+                <p className="mt-1">$21.15</p>
+                <p className="mt-1">S,M,L</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative  rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/necklace/nomodel.webp"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Necklace
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Neptune
+                </h2>
+                <p className="mt-1">$12.00</p>
+                <p className="mt-1">S,M,L</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <a className="block relative  rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src="/necklace/nomodel.webp"
+                />
+              </a>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Necklace
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The 400 Blows
+                </h2>
+                <p className="mt-1">$18.40</p>
+                <p className="mt-1">S,M,L</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Necklace;
