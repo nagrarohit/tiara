bc654f086d62d61b3c41f844426d496de9c8c386
nagrarohit <rohit.nagra7861@gmail.com>
2023-09-20 23:52:01 +0530

    cart button updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
+
 import { HiShoppingCart } from "react-icons/hi";
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/Ai";
+import { TfiClose } from "react-icons/tfi";
 
 const Navbar = () => {
+  const toggleCart = () => {
+    if (ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-full");
+      ref.current.classList.add("translate-x-0");
+    } else if (!ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-0");
+      ref.current.classList.add("translate-x-full");
+    }
+  };
+  const ref = useRef();
   return (
     <div className=" flex  flex-col md:flex-row md:justify-normal justify-center items-center py-2 shadow-xl">
       <div className="logo mx-5">
@@ -24,8 +37,33 @@ const Navbar = () => {
           </Link>
         </ul>
       </div>
-      <div className="cart absolute right-0 top-9 mx-5 ">
+      <div
+        onClick={toggleCart}
+        className=" cursor-pointer cart absolute right-0 top-9 mx-5 "
+      >
         <HiShoppingCart className="text-xl md:text-4xl "></HiShoppingCart>
+      </div>
+      <div
+        ref={ref}
+        className=" w-72 sideCart absolute top-0 right-0 px-8 py-10 bg-purple-100  transform transition-transform translate-x-full"
+      >
+        <h1 className="font-bold text-xl">Shopping cart</h1>
+        <span
+          onClick={toggleCart}
+          className="absolute top-4 right-3 cursor-pointer text-2xl "
+        >
+          <TfiClose />
+        </span>
+        <ol className=" font-semibold list-decimal">
+          <li>
+            <div className="item flex my-3">
+              <div className="w-2/3 ">Necklace wear the best</div>
+              <div className="w-1/3 flex items-center justify-center">
+                <AiOutlinePlusCircle />1<AiOutlineMinusCircle />
+              </div>
+            </div>
+          </li>
+        </ol>
       </div>
     </div>
   );
