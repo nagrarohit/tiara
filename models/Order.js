d23a38a5f3118cecffef50c9c339b4a080365df4
nagrarohit <rohit.nagra7861@gmail.com>
2023-10-04 21:46:32 +0530

    Mongodb and Schema Intiated

---
+++ models/Order.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: { type: String, requiredd: true },
+    products: [
+      { productId: { type: String }, quantity: { type: Number, default: 1 } },
+    ],
+    address: { type: String, required: true },
+    amount: { type: Number, required: true },
+    status: { type: String, default: "pending", required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", OrderSchema);
