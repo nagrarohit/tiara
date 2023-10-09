caa99c8ce76da45672936ebff3307604f1ad9fc6
nagrarohit <rohit.nagra7861@gmail.com>
2023-10-09 22:08:24 +0530

    removed hardcoded data and fetched from mongodb

--- pages/api/getProducts.js
+++ pages/api/getProducts.js
@@ -3,6 +3,29 @@ import connectDb from "@/middleware/mongoose";
 
 const handler = async (req, res) => {
   let products = await Product.find();
-  res.status(200).json({ products });
+  let rings = {};
+  for (let item of products) {
+    if (item.title in rings) {
+      if (
+        !rings[item.title].color.includes(item.color) &&
+        item.availableQty > 0
+      ) {
+        rings[item.title].color.push(item.color);
+      }
+      if (
+        !rings[item.title].size.includes(item.size) &&
+        item.availableQty > 0
+      ) {
+        rings[item.title].size.push(item.size);
+      }
+    } else {
+      rings[item.title] = JSON.parse(JSON.stringify(item));
+      if (item.availableQty > 0) {
+        rings[item.title].color = [item.color];
+        rings[item.title].size = [item.size];
+      }
+    }
+  }
+  res.status(200).json({ rings });
 };
 export default connectDb(handler);
--- pages/rings.js
+++ pages/rings.js
@@ -1,205 +1,59 @@
 import React from "react";
 import { useRouter } from "next/router";
+import Product from "@/models/Product";
+import mongoose from "mongoose";
 
-const Rings = () => {
+const Rings = ({ products }) => {
   const router = useRouter();
-  const handleLinkClick = () => {
-    router.push("/product/wear-the-necklace");
+  const handleLinkClick = (item) => {
+    router.push(`/product/${item.slug}`);
   };
   return (
     <div>
       <section className="text-gray-600 body-font ">
         <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap -m-4">
-            <div
-              onClick={handleLinkClick}
-              style={{ cursor: "pointer" }}
-              className="lg:w-1/4 md:w-1/2 p-4 w-full "
-            >
-              <a className="block relative  rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block "
-                  src="/rings/ringcloth.webp "
-                />
-              </a>
-              <div className="mt-4 text-center">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  Necklace
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The Catalyzer
-                </h2>
-                <p className="mt-1">$16.00</p>
-                <p className="mt-1">S,M,L</p>
-              </div>
-            </div>
-            <div
-              onClick={handleLinkClick}
-              style={{ cursor: "pointer" }}
-              className="lg:w-1/4 md:w-1/2 p-4 w-full "
-            >
-              <a className="block relative  rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="/rings/ringcloth.webp"
-                />
-              </a>
-              <div className="mt-4 text-center">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  Necklace
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Shooting Stars
-                </h2>
-                <p className="mt-1">$21.15</p>
-                <p className="mt-1">S,M,L</p>
-              </div>
-            </div>
-            <div
-              onClick={handleLinkClick}
-              style={{ cursor: "pointer" }}
-              className="lg:w-1/4 md:w-1/2 p-4 w-full "
-            >
-              <a className="block relative  rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="/rings/ringcloth.webp"
-                />
-              </a>
-              <div className="mt-4 text-center">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  Necklace
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Neptune
-                </h2>
-                <p className="mt-1">$12.00</p>
-                <p className="mt-1">S,M,L</p>
-              </div>
-            </div>
-            <div
-              onClick={handleLinkClick}
-              style={{ cursor: "pointer" }}
-              className="lg:w-1/4 md:w-1/2 p-4 w-full "
-            >
-              <a className="block relative  rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="/rings/ringcloth.webp"
-                />
-              </a>
-              <div className="mt-4 text-center">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  Necklace
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The 400 Blows
-                </h2>
-                <p className="mt-1">$18.40</p>
-                <p className="mt-1">S,M,L</p>
-              </div>
-            </div>
-            <div
-              onClick={handleLinkClick}
-              style={{ cursor: "pointer" }}
-              className="lg:w-1/4 md:w-1/2 p-4 w-full "
-            >
-              <a className="block relative  rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="/rings/ringcloth.webp"
-                />
-              </a>
-              <div className="mt-4 text-center">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  Necklace
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The Catalyzer
-                </h2>
-                <p className="mt-1">$16.00</p>
-                <p className="mt-1">S,M,L</p>
-              </div>
-            </div>
-            <div
-              onClick={handleLinkClick}
-              style={{ cursor: "pointer" }}
-              className="lg:w-1/4 md:w-1/2 p-4 w-full "
-            >
-              <a className="block relative  rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="/rings/ringcloth.webp"
-                />
-              </a>
-              <div className="mt-4 text-center">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  Necklace
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Shooting Stars
-                </h2>
-                <p className="mt-1">$21.15</p>
-                <p className="mt-1">S,M,L</p>
-              </div>
-            </div>
-            <div
-              onClick={handleLinkClick}
-              style={{ cursor: "pointer" }}
-              className="lg:w-1/4 md:w-1/2 p-4 w-full "
-            >
-              <a className="block relative  rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="/rings/ringcloth.webp"
-                />
-              </a>
-              <div className="mt-4 text-center">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  Necklace
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Neptune
-                </h2>
-                <p className="mt-1">$12.00</p>
-                <p className="mt-1">S,M,L</p>
-              </div>
-            </div>
-            <div
-              onClick={handleLinkClick}
-              style={{ cursor: "pointer" }}
-              className="lg:w-1/4 md:w-1/2 p-4 w-full "
-            >
-              <a className="block relative  rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="/rings/ringcloth.webp"
-                />
-              </a>
-              <div className="mt-4 text-center">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  Necklace
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The 400 Blows
-                </h2>
-                <p className="mt-1">$18.40</p>
-                <p className="mt-1">S,M,L</p>
-              </div>
-            </div>
+          <div className="flex flex-wrap -m-4 justify-center">
+            {products.map((item) => {
+              return (
+                <div
+                  key={item._id}
+                  onClick={() => handleLinkClick(item)}
+                  style={{ cursor: "pointer" }}
+                  className="lg:w-1/4 md:w-1/2 p-4 w-full "
+                >
+                  <a className="block relative  rounded overflow-hidden">
+                    <img
+                      alt="ecommerce"
+                      className="object-cover object-center w-full h-full block "
+                      src={item.img}
+                    />
+                  </a>
+                  <div className="mt-4 text-center">
+                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                      Ring
+                    </h3>
+                    <h2 className="text-gray-900 title-font text-lg font-medium">
+                      {item.title}
+                    </h2>
+                    <p className="mt-1">${item.price}</p>
+                    <p className="mt-1">{item.size}</p>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </section>
     </div>
   );
 };
-
+export async function getServerSideProps(context) {
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI);
+  }
+  let products = await Product.find({ category: "ring" });
+  return {
+    props: { products: JSON.parse(JSON.stringify(products)) },
+  };
+}
 export default Rings;
