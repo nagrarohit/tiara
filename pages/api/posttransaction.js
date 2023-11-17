7adbfa243b40cd60c7d834a1739be00725f9df23
nagrarohit <rohit.nagra7861@gmail.com>
2023-11-17 17:43:58 +0530

    order and orders page for user

--- pages/api/posttransaction.js
+++ pages/api/posttransaction.js
@@ -5,18 +5,18 @@ const handler = async (req, res) => {
   //validate paytm checksum
   //update status into orders table after checking the transaction status
   if (req.body.STATUS == "TXN_SUCCESS") {
-    await Order.findOneAndUpdate(
+    let order = await Order.findOneAndUpdate(
       { orderId: req.body.ORDERID },
       { status: "paid", paymentInfo: JSON.stringify(req.body) }
     );
   } else if (req.body.status == "PENDING") {
-    await Order.findOneAndUpdate(
+    let order = await Order.findOneAndUpdate(
       { orderId: req.body.ORDERID },
       { status: "pending", paymentInfo: JSON.stringify(req.body) }
     );
   }
   //Initiate shipping
   //Redirect user to the order confirmation page
-  res.redirect("/order", 200);
+  res.redirect("/order?id=" + order._id, 200);
 };
 export default connectDb(handler);
--- pages/order.js
+++ pages/order.js
@@ -1,37 +1,44 @@
+import { useRouter } from "next/router";
+import Order from "@/models/Order";
 import React from "react";
+import mongoose from "mongoose";
 
-const order = () => {
+const MyOrder = ({ order }) => {
+  const products = order.products;
+  console.log(order);
+
   return (
     <section className="text-gray-600 body-font overflow-hidden">
       <div className="container px-5 py-24 mx-auto">
         <div className="lg:w-4/5 mx-auto flex flex-wrap">
           <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
             <h2 className="text-lg title-font text-black tracking-widest font-extrabold">
-              Tiara16.com
+              Tiara16
             </h2>
             <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">
-              Order Id: #43857983475
+              Order Id: #{order.orderId}
             </h1>
 
             <p className="leading-relaxed mb-4">
               Your order has been successfully placed. Thanks for shopping with
-              us. Keep buying for exciting offers and best deals!
+              us.
+              <p>Your payment Status is {order.status}</p>
             </p>
             <div className="flex border-t border-gray-200 py-2">
               <span className="text-gray-500">Color</span>
-              <span className="ml-auto text-gray-900">Blue</span>
+              <span className="ml-auto text-gray-900">{order.variant}</span>
             </div>
             <div className="flex border-t border-gray-200 py-2">
               <span className="text-gray-500">Size</span>
-              <span className="ml-auto text-gray-900">Medium</span>
+              <span className="ml-auto text-gray-900">{order.size}</span>
             </div>
             <div className="flex border-t border-b mb-6 border-gray-200 py-2">
               <span className="text-gray-500">Quantity</span>
-              <span className="ml-auto text-gray-900">4</span>
+              <span className="ml-auto text-gray-900">{order.qty}</span>
             </div>
             <div className="flex">
               <span className="title-font font-medium text-2xl text-gray-900">
-                Subtotal: $58.00
+                Subtotal: ${order.amount}
               </span>
               <button className="flex ml-auto text-white bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded">
                 Track Order
@@ -49,4 +56,17 @@ const order = () => {
   );
 };
 
-export default order;
+export async function getServerSideProps(context) {
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI);
+  }
+  let order = await Order.findById(context.query.id);
+
+  return {
+    props: {
+      order: JSON.parse(JSON.stringify(order)),
+    },
+  };
+}
+
+export default MyOrder;
