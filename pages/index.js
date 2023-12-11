8d96a8489dc8ae7b86c706168ebfb16dbede0074
nagrarohit <rohit.nagra7861@gmail.com>
2023-12-11 19:12:09 +0530

    overflow and home text

--- pages/index.js
+++ pages/index.js
@@ -201,7 +201,7 @@ export default function Home() {
               <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
             </svg>
             <blockquote>
-              <p className="text-2xl italic font-medium text-gray-900 dark:text-white">
+              <p className="text-2xl italic font-medium text-gray-900 dark:text-gray-700">
                 "Adorn yourself with elegance, embrace the allure of Tiara –
                 where every piece tells a story of timeless beauty and refined
                 craftsmanship."
@@ -209,7 +209,7 @@ export default function Home() {
             </blockquote>
             <figcaption className="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse">
               <div className="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-500 dark:divide-gray-700">
-                <cite className="pe-3 font-medium text-gray-900 dark:text-white">
+                <cite className="pe-3 font-medium text-gray-900 dark:text-gray-700">
                   nagrarohit
                 </cite>
                 <cite className="ps-3 text-sm text-gray-500 dark:text-gray-400">
